b6938fc77d030b0448542a0105781179312bb174
Germain <germain.hugon@liip.ch>
2015-10-08 15:39:43 +0200

    Implement the back button

--- index.android.js
+++ index.android.js
@@ -10,6 +10,8 @@ var {
   StyleSheet,
   Text,
   View,
+  BackAndroid,
+  Navigator,
 } = React;
 
 var LoginScreen = require('./src/screens/Login/LoginScreen');
@@ -36,15 +38,30 @@ var {CircularProgressAnimationExample} = require('./src/GuessUIExamples');
 var ResultScreenExample = require('./src/screens/Play/ResultScreenExample');
 var LaunchView = require('./src/UI/LaunchView');
 
+BackAndroid.addEventListener('hardwareBackPress', () => {
+  if (_navigator.getCurrentRoutes().length === 1) {
+    return false;
+  }
 
+  _navigator.pop();
+  return true;
+});
 
-
-
-var StartComponent = PlayingNoAnswer;
+var StartComponent = LoginScreen;
+var _navigator;
 
 var App = React.createClass({
+  renderScene: function(route, navigator) {
+    _navigator = navigator;
+    return <route.component navigator={navigator} />;
+  },
+
   render: function() {
-    return <StartComponent />
+    return (
+      <Navigator
+        initialRoute = {{id: 'Home', component: LoginScreen }}
+        configureScene={() => Navigator.SceneConfigs.FadeAndroid}
+        renderScene={this.renderScene} />);
   }
 });
 

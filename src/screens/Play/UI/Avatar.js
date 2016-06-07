02b9889150c982def22c8e99ad6df3e005918bc7
syzer3@gmail.com <syzer3@gmail.com>
2016-06-07 21:13:04 +0200

    Avatar es6

--- src/screens/Play/UI/Avatar.js
+++ src/screens/Play/UI/Avatar.js
@@ -1,50 +1,50 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {
-    View,
-    Image,
-    StyleSheet,
+  View,
+  Image,
+  StyleSheet,
 } from 'react-native'
 
 /**
  * An avatar shows a picture of a person with rounded corners.
  */
-var Avatar = React.createClass({
+class Avatar extends Component {
 
-    propTypes: {
-        picture: React.PropTypes.string.isRequired,
-        diameter: React.PropTypes.number.isRequired,
-        onLoad: React.PropTypes.func.isRequired,
-    },
-
-    render: function() {
-        var dimensions = {
-            width: this.props.diameter,
-            height: this.props.diameter,
-            // Use the border radius to cut the image
-            // into a perfect circle.
-            borderRadius: this.props.diameter / 2,
-        };
-
-        return (
-            <View>
-                <Image
-                    source={{uri: this.props.picture }}
-                    style={[styles.avatar, dimensions]}
-                    onLoad={this.props.onLoad}>
-                    {this.props.children}
-                </Image>
-            </View>
-        );
+  static get propTypes() {
+    return {
+      picture: React.PropTypes.string.isRequired,
+      diameter: React.PropTypes.number.isRequired,
+      onLoad: React.PropTypes.func.isRequired,
     }
-});
+  }
 
+  render() {
+    var dimensions = {
+      width: this.props.diameter,
+      height: this.props.diameter,
+      // Use the border radius to cut the image
+      // into a perfect circle.
+      borderRadius: this.props.diameter / 2,
+    }
 
-// Styles
+    return (
+      <View>
+        <Image
+          source={{uri: this.props.picture }}
+          style={[styles.avatar, dimensions]}
+          onLoad={this.props.onLoad}>
+          {this.props.children}
+        </Image>
+      </View>
+    )
+  }
+}
+
 var styles = StyleSheet.create({
-    avatar: {
-        borderWidth: 1,
-        borderColor: 'transparent'
-    }
-});
+  avatar: {
+    borderWidth: 1,
+    borderColor: 'transparent',
+  }
+})
 
-module.exports = Avatar;
+module.exports = Avatar
